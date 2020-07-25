060d08fb4169e977b460901d7277f21ec7de9da6
duttasaptarshi <68784363+duttasaptarshi@users.noreply.github.com>
2020-07-25 21:53:43 +0530

    Update basicrouting.php

--- basicrouting.php
+++ basicrouting.php
@@ -13,3 +13,24 @@ Route::get('/', function () {
     'name' => $name
     ]);
 });
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta chatset ="UTF-8">
+    <meta
+        name="viewport"
+        content="width=device=width,user=scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0"
+    >
+    <meta
+        http-equiv="X-UA-compatible"
+        content="ie=edge"
+    >
+    <title>Document</title>
+</head>
+<body>
+<hl>{{ $name }}</hl>
+</body>
+</html>
+
+
