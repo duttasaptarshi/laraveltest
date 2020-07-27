8bdfb6b679ea3ecf4400cd9129b8203fbfa4220d
duttasaptarshi <68784363+duttasaptarshi@users.noreply.github.com>
2020-07-27 17:05:58 +0530

    Update database_access.php

--- database_access.php
+++ database_access.php
@@ -52,3 +52,32 @@ class postcontroller extends controller
         ]);   
     }
 }
+
+//command for migration(for table creation)
+php artisan migrate
+php artisan make:migration create_posts_table
+//a php file is created in migration
+//we edit the file and fill the table coloumn
+//DATABASE/MIGRATION/2020_07_27_110504_create_posts_table.php
+ <?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+class CreatePostsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('slug');
+            $table->string('title');
+            $table->text('body');
+            $table->timestamps();
+            $table->timestamp('published_at')->nullable();
+        });
+    }
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
