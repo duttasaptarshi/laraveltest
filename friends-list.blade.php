1b8e8a184aff8887d97f33d474a07a98e218b00f
duttasaptarshi <68784363+duttasaptarshi@users.noreply.github.com>
2020-08-08 21:42:03 +0530

    Add files via upload

--- friends-list.blade.php
+++ friends-list.blade.php
@@ -1,16 +1,22 @@
-<h3 class="font-bold text-xl mb=4">Friends</h3>
+<div class ="bg-green-200 rounded-lg py-4 px-6">
+    <h3 class="font-bold text-xl mb=4">Following</h3>
+    <br>
 
-<ul>
-    @foreach(range(1 , 8) as $index)
-        <li class="mb-4">
-            <div class="flex items-center text-sm">
-                <img
-                    src="https://i.pravatar.cc/40"
-                    alt="" 
-                    class="rounded-full mr-2"           
-                >
-                Sappy
-            </div>
-        </li>
-    @endforeach
-</ul>
+    <ul>
+        @foreach(auth()->user()->follows as $user)
+            <li class="mb-4">
+                <div>
+                    <a href="{{ route('profile' , $user) }}" class="flex items-center text-sm">
+                        <img
+                            src="{{ $user->avatar }}"
+                            alt="" 
+                            class="rounded-full mr-2"
+                            width="40" height="40"           
+                        >
+                        {{ $user->name }}
+                    </a>
+                </div>
+            </li>
+        @endforeach
+    </ul>
+</div>
--- publish-tweet-panel.blade.php
+++ publish-tweet-panel.blade.php
@@ -1,6 +1,8 @@
 
-<div class="border border-blue-400 rounded-lg px-8 py-6">
-    <form action="">
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
+    <form method="POST" action="/tweets">
+        @csrf
+
         <textarea
             name="body"
             class="w-full"
@@ -11,11 +13,15 @@
 
         <footer class="flex justify-between">
             <img
-                src="https://i.pravatar.cc/40"
-                alt="" 
+                src="{{ auth()->user()->avatar }}"
+                alt="your avatar" 
                 class="rounded-full mr-2"           
             >
             <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">tweet-a-roo!</button>
         </footer>
     </form>
+
+    @error('body')
+     <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
+    @enderror
 </div>
